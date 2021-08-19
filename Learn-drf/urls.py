fb91eed34a5caae069308276dfdda2ae9eb27b26
Ankitgupta16 <87710931+Ankitgupta16@users.noreply.github.com>
2021-08-19 18:33:34 +0530

    Update urls.py

--- Learn-drf/urls.py
+++ Learn-drf/urls.py
@@ -13,8 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -22,6 +21,7 @@ from .router import router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('',include('store.urls')),
+    path('api/', include(router.urls)),
 
 ]
